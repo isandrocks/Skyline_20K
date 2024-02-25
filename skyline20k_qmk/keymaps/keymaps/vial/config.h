871c4f22a3c1af434c4dbd71ec436da8d7c51c4e
iSandRocks <sean.h1313@gmail.com>
2024-02-25 06:40:28 -0500

    Vial support added

---
+++ skyline20k_qmk/keymaps/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x36, 0x59, 0xE4, 0xAB, 0x14, 0xEF, 0xFC, 0xA2}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
---
+++ skyline20k_qmk/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x54, 0x62, 0x44, 0xE8, 0x95, 0x8C, 0x33, 0x60}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
